97922f5bed8c1340fcdf194ce35ff41aec6ff98a
DavidJS01 <davidshipmanchs2020@gmail.com>
2022-11-29 05:18:42 -0700

    ci: ci

--- go-twitch-events/rabbitmq/main_test.go
+++ go-twitch-events/rabbitmq/main_test.go
@@ -11,7 +11,7 @@ func TestConnectToRabbitMQ(t *testing.T) {
 	if os.Getenv("INTEGRATION") == "" {
 		t.Skip("Skipping integration test")
 	}
-	t.Setenv("AMQP_SERVER_URL", os.Getenv("rabbitMQAddress"))
+	t.Setenv("AMQP_SERVER_URL", os.Getenv("AMQP_SERVER_URL"))
 	conn := ConnectToRabbitMQ()
 	fmt.Print(conn.RemoteAddr())
 	
